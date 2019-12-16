8681d4be308d3ccfcb23a3a73ab98855b847d6f3
BB. Jansen <bobjansen@pm.me>
2019-12-16 20:51:38 +0100

    formatting

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-<footer class="section">
-  <div class="content has-text-centered">
-    <p>
-     <strong>LTO Services</strong> &copy; 2019. The website content and source code
-      is licensed by <a href="https://opensource.org/licenses/AGPL-3.0">AGPL-3.0</a>.
-    </p>
-  </div>
-</footer>
+  <footer class="section">
+    <div class="content has-text-centered">
+      <p>
+        <strong>LTO Services</strong> &copy; 2019. The website content and source code
+        is licensed by <a href="https://opensource.org/licenses/AGPL-3.0">AGPL-3.0</a>.
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
