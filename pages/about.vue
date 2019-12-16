019b1863c73dffc27d6e133b80c2aa5a37d8dd28
BB. Jansen <bobjansen@pm.me>
2019-12-16 20:53:29 +0100

    about page foundation

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="notification is-dark">
+      <p class="title">
+        {{ $t('tile.about.title') }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'About',
+  components: {
+
+  }
+}
+</script>
