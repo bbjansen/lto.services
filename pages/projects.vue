82a37d47286cea4d31151b61fd591aa8b7c03188
BB. Jansen <bobjansen@pm.me>
2019-12-17 04:31:50 +0100

    pull projects from ghost

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,23 +1,32 @@
 <template>
   <section class="section is-paddingless">
     <div class="tile is-ancestor" style="padding-top: 40px;">
-      <div class="tile is-vertical">
-        <div class="tile">
-          <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-dark is-radiusless">
+      <div class="tile">
+        <div
+          class="tile"
+          v-for="post in posts"
+          v-bind:key="post"
+        >
+          <div class="tile is-parent">
+            <article
+              class="tile is-child notification is-dark is-radiusless"
+            >
               <p class="title">
-                Decentralized Automated Pool
+                {{ post.title }}
               </p>
-              <b-taglist>
-                <b-tag
-                  v-for="item in $t('tile.consult.services')"
-                  v-bind:key="item"
-                  size="is-medium"
-                  type="is-white"
-                >
-                  {{ item }}
-                </b-tag>
-              </b-taglist>
+
+              <p class="subtitle">
+                {{ post.excerpt }}
+              </p>
+              <b-button
+                :href="post.url"
+                tag="a"
+                type="is-white"
+                expanded
+                outlined
+              >
+                Read More
+              </b-button>
             </article>
           </div>
         </div>
@@ -27,12 +36,21 @@
 </template>
 
 <script>
+import GhostContentAPI from '@tryghost/content-api'
+
+const api = new GhostContentAPI({
+  url: 'https://cms.lto.services',
+  key: 'ce375a1758e6ca83e6a7889c16',
+  version: 'v3'
+})
 
 export default {
   name: 'Projects',
-
   components: {
-
+  },
+  async asyncData () {
+    const posts = await api.posts.browse({ slug: 'projects' })
+    return { posts }
   }
 }
 </script>
